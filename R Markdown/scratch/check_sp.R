1251258899cd6f574f5f3418236e736bc5797ba1
Gabs_Planner <99758447+gabrielmachadoplanejador@users.noreply.github.com>
2026-05-28 10:27:34 -0300

    Delete R Markdown directory

--- R Markdown/scratch/check_sp.R
+++
@@ -1,5 +0,0 @@
-library(dplyr)
-pasta_dados <- "dados"
-arquivo_base_central <- file.path(pasta_dados, "tabela_conferencia_completa.xlsx")
-df <- readxl::read_excel(arquivo_base_central)
-print(unique(df$subprefeitura))
